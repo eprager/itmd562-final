c56e9bdb07f7dbcceb30651636dcd3cda80ca019
Emma Prager <eprager@hawk.iit.edu>
2019-11-24 22:37:02 -0600

    add js for date

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+  var d = new Date();
+  var date = d.getDate();
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var month = months[d.getMonth()];
+  var year = d.getFullYear();
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var day = days[d.getDay()];
+  var today = day + ", " + month + " " + date + ", " + year;
+  document.getElementById("thedate").innerHTML = today;
+});
